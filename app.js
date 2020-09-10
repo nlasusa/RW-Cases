aef3834c35886adec0d9c7159beae6c9d39f95b4
Nicole Lasusa <nicolelasusa@Nicoles-Air.fios-router.home>
2020-09-10 11:06:03 -0400

    alphabet cases

--- app.js
+++ app.js
@@ -8,5 +8,19 @@ $(document).ready(function(){
 
 // ON CLICK EVENTS 
 $("#learn-more-1").on('click', function(){
-    window.location = "https://ktmc.com/";    
+    window.location = "https://ktmc.com/new-cases/alteryx-inc#overview";    
   });
+
+  $("#a").on('click', function(){
+   $("#alpha-A").show();
+   $("#alpha-B").hide();
+ });
+
+ $("#b").on('click', function(){
+   $("#alpha-B").show();
+   $("#alpha-A").hide();
+ });
+
+
+ $("#alpha-A").hide();
+ $("#alpha-B").hide();
